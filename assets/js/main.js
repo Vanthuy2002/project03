e5e1f5aecd8a0ea265f03cf772a16056b9edc6dd
Thuy Nguyen <vanthuy101102@gmail.com>
2023-03-26 11:42:59 +0700

    clone how it work

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,13 @@ navLinks.forEach((link) => {
     link.classList.add("navbar__item--active");
   });
 });
+
+let workItems = document.querySelectorAll(".work__item");
+workItems.forEach((item) => {
+  item.addEventListener("mousemove", () => {
+    [...workItems].forEach((value) => {
+      value.classList.remove("work__item--active");
+    });
+    item.classList.add("work__item--active");
+  });
+});
